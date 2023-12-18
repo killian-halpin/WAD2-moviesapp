a79333d6752092ceaa558a8626fde4587d42504c
Killian Halpin <you@example.com>
2023-12-18 16:33:23 +0000

    API added - some errors to deal with

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import sample from './stpries/sampleData';
+import MoviePage from "./pages/movieDetailsPage";
+
+const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+];
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/" element={<HomePage movies={movies} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
