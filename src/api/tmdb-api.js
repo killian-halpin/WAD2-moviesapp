9bc72ef4ea873ea6400c1c7c7f8f730ec8fe4353
Killian Halpin <you@example.com>
2024-11-25 11:11:59 +0000

    Commit

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -74,19 +74,15 @@ export const getMovieReviews = (id) => {
       return json.results;
     });
 };
-
-export const getUpcomingMovies = () => {
-  return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.json().message);
-    }
-    return response.json();
-  })
-  .catch((error) => {
-     throw error
+export const getUpcomingMovies = async () => {
+  const response = await fetch('http://localhost:8080/tmdb/upcoming', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      }
   });
+  
+  return response.json();
 };
 
 export const getPopularMovies = () => {
